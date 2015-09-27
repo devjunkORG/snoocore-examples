b3fb250003a3feddf755a1b6962698ee6e6bb298
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-09-27 22:26:55 +0200

    Fix missing cookieparser dependency.
    req.cookies depends on cookieparser middleware.
    
    Fix contains following bits:
    
    - Add cookieparser dependency to packages.json.
    - Ensure cookieparser is loaded in express-sample.

--- express-web/app.js
+++ express-web/app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
+
 var Snoocore = require('snoocore');
 
 var uniqId = 0; // unique id for our user
